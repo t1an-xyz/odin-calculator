8aecdecd29da7ffd7dd84da882e3c8e74a74ed29
Theo <62950302+t1an-xyz@users.noreply.github.com>
2023-12-18 21:59:17 -0500

    binary expressions

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const display = document.getElementById('display');
+let left = 0;
+let op = '';
+let clearFlag = false;
 
 let reset = function() {
     display.innerText = '0';
+    left = 0;
+    op = '';
+    clearFlag = false;
 }
 
 let addNumber = function(number) {
@@ -17,18 +23,70 @@ let backSpace = function() {
     if (displayText.length > 1) {
         display.innerText = displayText.substring(0, displayText.length - 1);
     } else {
-        reset();
+        display.innerText = '0';
     }
+}
+
+let evaluate = function() {
+    let right = parseFloat(display.innerText);
+    let result = 0;
+    switch (op) {
+        case 'add':
+            result = left + right;
+            break;
+        case 'subtract':
+            result = left - right;
+            break;
+        case 'multiply':
+            result = left * right;
+            break;
+        case 'divide':
+            result = left / right;
+            break;
+    }
+    op = '';
+    left = result;
 }
 
 document.getElementById('buttons').addEventListener('click', function(event) {
     let element = event.target;
     if (element.classList.contains('number')) {
+        if (left && op === '') {
+            reset();
+        }
+        if (op !== '') {
+            clearFlag = true;
+        }
+        if (clearFlag) {
+            display.innerText = '0';
+            clearFlag = false;
+        }
         addNumber(element.innerText);
     } else if (element.id === 'clear') {
         reset();
     } else if (element.id === 'backspace') {
+        if (clearFlag) {
+            display.innerText = '0';
+            clearFlag = false;
+        }
         backSpace();
+    } else if (element.classList.contains('op')) {
+        if (op && clearFlag) {
+            evaluate();
+            display.innerText = left;
+        } else {
+            left = parseFloat(display.innerText);
+        }
+        op = element.id;
+    } else if (element.id === 'equals') {
+        if (op !== '' && !clearFlag) {
+            evaluate();
+            display.innerText = left;
+            clearFlag = true;
+        } else {
+            clearFlat = true;
+            display.innerText = 'Syntax Error';
+        }
     }
 });
 
